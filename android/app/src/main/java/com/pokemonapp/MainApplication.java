5703f2baa292bdd12f809e35799c9309bd09af69
Diah Novianti <diahnovianti0711@gmail.com>
2022-06-16 21:29:17 +0700

    pullrequest sonar cloud demo

--- android/app/src/main/java/com/pokemonapp/MainApplication.java
+++ android/app/src/main/java/com/pokemonapp/MainApplication.java
@@ -16,7 +16,7 @@ import com.facebook.soloader.SoLoader;
 import com.pokemonapp.newarchitecture.MainApplicationReactNativeHost;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.microsoft.codepush.react.CodePush;
+// import com.microsoft.codepush.react.CodePush;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -41,10 +41,10 @@ public class MainApplication extends Application implements ReactApplication {
           return "index";
         }
 
-         @Override
-        protected String getJSBundleFile() {
-            return CodePush.getJSBundleFile();
-        }
+        //  @Override
+        // protected String getJSBundleFile() {
+        //     return CodePush.getJSBundleFile();
+        // }
       };
 
   private final ReactNativeHost mNewArchitectureNativeHost =
